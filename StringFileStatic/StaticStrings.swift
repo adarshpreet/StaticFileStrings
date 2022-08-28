2665e0da9c2f6cd81552b3588a9b10afc6815bde
Adarshpreet kaur <adarshpreetkaur@AdarshpeetsMini.Optilink>
2022-08-28 16:49:41 +0530

    Initial project setup

---
+++ StringFileStatic/StaticStrings.swift
@@ -0,0 +1,15 @@
+//
+//  StaticStrings.swift
+//  StringFileStatic
+//
+//  Created by Adarshpreet kaur on 28/08/22.
+//
+
+import Foundation
+public class StaticStrings {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
---
+++ StringFileStatic/StringFileStatic.h
@@ -0,0 +1,18 @@
+//
+//  StringFileStatic.h
+//  StringFileStatic
+//
+//  Created by Adarshpreet kaur on 28/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringFileStatic.
+FOUNDATION_EXPORT double StringFileStaticVersionNumber;
+
+//! Project version string for StringFileStatic.
+FOUNDATION_EXPORT const unsigned char StringFileStaticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringFileStatic/PublicHeader.h>
+
+
